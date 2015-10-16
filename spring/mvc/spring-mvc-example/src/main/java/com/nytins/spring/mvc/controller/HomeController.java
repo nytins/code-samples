993b901eb4b4890d1adbef6621682e307fa7559a
Nithin Satheesan <nytins@gmail.com>
2015-10-15 22:59:27 -0400

    Add service.

--- spring/mvc/spring-mvc-example/src/main/java/com/nytins/spring/mvc/controller/HomeController.java
+++ spring/mvc/spring-mvc-example/src/main/java/com/nytins/spring/mvc/controller/HomeController.java
@@ -1,5 +1,21 @@
 package com.nytins.spring.mvc.controller;
 
-public class HomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.nytins.spring.mvc.service.NameService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private NameService nameService;
+	
+	@RequestMapping("/")
+	public String homePage(ModelMap model) {
+		model.addAttribute("name", nameService.getName());
+		return "home";
+	}
 }
