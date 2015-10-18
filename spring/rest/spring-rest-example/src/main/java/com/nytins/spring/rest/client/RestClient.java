49c0d732a3080e8d56c427bf60597f7126ea3819
Nithin Satheesan <nytins@gmail.com>
2015-10-17 23:44:07 -0400

    Add more rest operations and rest client.

--- spring/rest/spring-rest-example/src/main/java/com/nytins/spring/rest/client/RestClient.java
+++ spring/rest/spring-rest-example/src/main/java/com/nytins/spring/rest/client/RestClient.java
@@ -7,8 +7,15 @@ import com.nytins.spring.rest.model.User;
 public class RestClient {
 
 	public static void main(String[] args) {
-		User user = new RestTemplate().getForObject("http://localhost:8080/spring-rest-example/users/ammoos", 
-				User.class);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		User user = new User("neetus", "Nithin", "nytins@gmail.com");
+		
+//		restTemplate.postForObject("http://localhost:8080/spring-rest-example/users", user,	User.class);
+		restTemplate.put("http://localhost:8080/spring-rest-example/users/ammoos", user, User.class);
+		
+		user = restTemplate.getForObject("http://localhost:8080/spring-rest-example/users/ammoos", User.class);
+		
 		System.out.println(user);
 	}
 }
--- spring/rest/spring-rest-example/src/main/java/com/nytins/spring/rest/controller/UserController.java
+++ spring/rest/spring-rest-example/src/main/java/com/nytins/spring/rest/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.nytins.spring.rest.controller;
 
+import java.util.Collection;
+
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.nytins.spring.rest.model.User;
 import com.nytins.spring.rest.service.UserService;
@@ -18,6 +23,25 @@ public class UserController {
 	@Inject
 	private UserService userService;
 
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void createUser(@RequestBody User user) {
+		System.out.println("createUser - " + user);
+		userService.addUser(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateUser(@PathVariable String id, @RequestBody User user) {
+		System.out.println("updateUser - " + user);
+		userService.addUser(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody Collection<User> getAllUsers() {
+		return userService.getAllUsers();
+	}
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public @ResponseBody User getUser(@PathVariable String id) {
 		System.out.println("getUser - " + id);
